82275b5f0838026b2df23d75d7e9a70229d39e8b
laneser <laneser.kuo@gmail.com>
2022-03-09 17:24:34 +0000

    Fix: Add user space time measurement

--- client_statistic.c
+++ client_statistic.c
@@ -6,11 +6,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 
 #define FIB_DEV "/dev/fibonacci"
 #define sample_size 5000
 #define mode 1
+#define allmode mode * 2
 
 int main()
 {
@@ -28,32 +30,40 @@ int main()
      */
     for (int i = 0; i <= offset; i++) {
         lseek(fd, i, SEEK_SET);
-        double time[mode][sample_size] = {0};
-        double mean[mode] = {0.0}, sd[mode] = {0.0}, result[mode] = {0.0};
-        int count[mode] = {0};
+        double time[allmode][sample_size] = {0};
+        double mean[allmode] = {0.0}, sd[allmode] = {0.0},
+               result[allmode] = {0.0};
+        int count[allmode] = {0};
 
         for (int n = 0; n < sample_size; n++) { /* sampling */
             /* get the runtime in kernel space here */
             for (int m = 0; m < mode; ++m) {
-                time[m][n] = (double) write(fd, NULL, m);
-                mean[m] += time[m][n]; /* sum */
+                struct timespec t1, t2;
+                clock_gettime(CLOCK_MONOTONIC, &t1);
+                time[m * 2][n] = (double) write(fd, NULL, m);
+                clock_gettime(CLOCK_MONOTONIC, &t2);
+                time[m * 2 + 1][n] =
+                    (long long) (t2.tv_sec * 1e9 + t2.tv_nsec) -
+                    (t1.tv_sec * 1e9 + t1.tv_nsec);
+                mean[m * 2] += time[m * 2][n];         /* sum */
+                mean[m * 2 + 1] += time[m * 2 + 1][n]; /* sum */
             }
         }
-        for (int m = 0; m < mode; ++m) {
+        for (int m = 0; m < allmode; ++m) {
             mean[m] /= sample_size; /* mean */
         }
 
         for (int n = 0; n < sample_size; n++) {
-            for (int m = 0; m < mode; ++m) {
+            for (int m = 0; m < allmode; ++m) {
                 sd[m] += (time[m][n] - mean[m]) * (time[m][n] - mean[m]);
             }
         }
-        for (int m = 0; m < mode; ++m) {
+        for (int m = 0; m < allmode; ++m) {
             sd[m] = sqrt(sd[m] / (sample_size - 1)); /* standard deviation */
         }
 
         for (int n = 0; n < sample_size; n++) { /* remove outliers */
-            for (int m = 0; m < mode; ++m) {
+            for (int m = 0; m < allmode; ++m) {
                 if (time[m][n] <= (mean[m] + 2 * sd[m]) &&
                     time[m][n] >= (mean[m] - 2 * sd[m])) {
                     result[m] += time[m][n];
@@ -61,16 +71,16 @@ int main()
                 }
             }
         }
-        for (int m = 0; m < mode; ++m) {
+        for (int m = 0; m < allmode; ++m) {
             result[m] /= count[m];
         }
 
         fprintf(fp, "%d ", i);
-        for (int m = 0; m < mode; ++m) {
+        for (int m = 0; m < allmode; ++m) {
             fprintf(fp, "%.5lf ", result[m]);
         }
         fprintf(fp, "samples: ");
-        for (int m = 0; m < mode; ++m) {
+        for (int m = 0; m < allmode; ++m) {
             fprintf(fp, "%d ", count[m]);
         }
         fprintf(fp, "\n");
--- scripts/plot-statistic.gp
+++ scripts/plot-statistic.gp
@@ -5,5 +5,6 @@ set title 'Fibonacci runtime'
 set term png enhanced font 'Verdana,10'
 set output 'plot_statistic.png'
 set grid
-plot [0:92][0:600] \
-'plot_input_statistic' using 1:2 with linespoints linewidth 2 title "fib\\_sequence"
+plot [0:92][0:4000] \
+'plot_input_statistic' using 1:2 with linespoints linewidth 2 title "fib\\_sequence kernel",\
+'' using 1:3 with linespoints linewidth 2 title "fib\\_sequence user",\
